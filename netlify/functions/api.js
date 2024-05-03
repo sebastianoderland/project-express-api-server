d96c882878b9341d40845bf3add996f2b338cc0d
Sebastian Oderland <sebastian.oderland@oderland.se>
2024-05-03 11:06:16 +0200

    update
    Signed-off-by: Sebastian Oderland <sebastian.oderland@oderland.se>

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,15 +1,15 @@
-import express, { Router } from "express"
+import express from "express"
 import serverless from "serverless-http"
-import cors from "cors"
-import avocadoSalesData from "./data/avocado-sales.json"
-import listEndpoints from "express-list-endpoints"
-import serverless from "serverless-http"
+// import cors from "cors"
+// import avocadoSalesData from "./data/avocado-sales.json"
+// import listEndpoints from "express-list-endpoints"
+// import serverless from "serverless-http"
 
 const api = express()
 
-const router = Router()
-router.get("/hello", (req, res) => res.send("HelloaaaaaaaWorld!"))
+// const router = Router()
+api.get("/hello", (req, res) => res.send("HelloaaaaaaaWorld!"))
 
-api.use("/api/", router)
+// api.use("/api/", router)
 
 export const handler = serverless(api)
